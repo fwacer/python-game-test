395fb6ba7b8d368c39629c9c80957ff1a07a3e2e
fwacer <bryce.dombrowski@hotmail.com>
2024-01-25 18:37:04 -0800

    Formatting

--- start.py
+++ start.py
@@ -11,16 +11,20 @@ import pygame
 
 from data.main import main
 
-def setup_root_logger():
 
+def setup_root_logger():
     root_logger = logging.getLogger()
     log_level = logging.DEBUG
     root_logger.setLevel(log_level)
-    
-    format_str = '%(levelname)s:%(name)s: %(message)s (%(asctime)s; %(filename)s:%(lineno)d)'
+
+    format_str = (
+        "%(levelname)s:%(name)s: %(message)s (%(asctime)s; %(filename)s:%(lineno)d)"
+    )
     formatter = logging.Formatter(fmt=format_str, datefmt="%Y-%m-%d %H:%M:%S")
 
-    file_handler = logging.handlers.RotatingFileHandler('game.log', encoding='utf8', backupCount=1, maxBytes=100000)
+    file_handler = logging.handlers.RotatingFileHandler(
+        "game.log", encoding="utf8", backupCount=1, maxBytes=100000
+    )
     console_handler = logging.StreamHandler()
 
     for handler in [file_handler, console_handler]:
@@ -29,6 +33,7 @@ def setup_root_logger():
         root_logger.addHandler(handler)
 
     return root_logger
+
 
 if __name__ == "__main__":
     root_logger = setup_root_logger()
